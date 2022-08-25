243c459494791da5dba24067a4c72b5c7c7d8947
wsycarlos <wsycarlos@126.com>
2022-08-25 13:32:06 +0800

    zh-tw batch
    launcher batch for zh-tw

---
+++ auto_derby/launcher/launcher_zh-tw.cs
@@ -0,0 +1,224 @@
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace NateScarlet.AutoDerby
+{
+    public class Option
+    {
+        public string Label
+        { get; set; }
+
+        public string Value
+        { get; set; }
+    }
+
+    public class JobOptions : ObservableCollection<Option>
+    {
+        public JobOptions()
+        {
+            Add(new Option()
+            {
+                Label = "Nurturing",
+                Value = "nurturing",
+            });
+        }
+    }
+
+    public class DistanceOptions : ObservableCollection<Option>
+    {
+        public DistanceOptions()
+        {
+            Add(new Option()
+            {
+                Label = "Short",
+                Value = "short",
+            });
+            Add(new Option()
+            {
+                Label = "Miles",
+                Value = "miles",
+            });
+            Add(new Option()
+            {
+                Label = "Medium",
+                Value = "medium",
+            });
+            Add(new Option()
+            {
+                Label = "Long",
+                Value = "long",
+            });
+        }
+    }
+
+    public class DataContext : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private const string RegistryPath = @"Software\NateScarlet\auto-derby";
+
+        private RegistryKey key;
+
+        public DataContext()
+        {
+            this.key = Registry.CurrentUser.OpenSubKey(RegistryPath, true);
+            if (this.key == null)
+            {
+                this.key = Registry.CurrentUser.CreateSubKey(RegistryPath);
+            }
+
+            this.JobOptions1 = new JobOptions();
+            this.DistanceOptions1 = new DistanceOptions();
+        }
+        ~DataContext()
+        {
+            key.Dispose();
+        }
+
+        public string DefaultSingleModeChoicesDataPath;
+        public string SingleModeChoicesDataPath
+        {
+            get
+            {
+                return (string)key.GetValue("SingleModeChoicesDataPath", DefaultSingleModeChoicesDataPath);
+            }
+            set
+            {
+                key.SetValue("SingleModeChoicesDataPath", value);
+                OnPropertyChanged("SingleModeChoicesDataPath");
+            }
+        }
+
+        public string DefaultPythonExecutablePath;
+        public string PythonExecutablePath
+        {
+            get
+            {
+                return (string)key.GetValue("PythonExecutablePath", DefaultPythonExecutablePath);
+            }
+            set
+            {
+                key.SetValue("PythonExecutablePath", value);
+                OnPropertyChanged("PythonExecutablePath");
+            }
+        }
+
+        public int PauseIfRaceOrderGt
+        {
+            get
+            {
+                return (int)key.GetValue("PauseIfRaceOrderGt", 5);
+            }
+            set
+            {
+                key.SetValue("PauseIfRaceOrderGt", value, RegistryValueKind.DWord);
+                OnPropertyChanged("PauseIfRaceOrderGt");
+            }
+        }
+
+        public string Plugins
+        {
+            get
+            {
+                return (string)key.GetValue("Plugins", "");
+            }
+            set
+            {
+                key.SetValue("Plugins", value);
+                OnPropertyChanged("Plugins");
+            }
+        }
+        
+        public string TargetTrainingValues
+        {
+            get
+            {
+                return (string)key.GetValue("TargetTrainingValues", "600,600,600,400,400");
+            }
+            set
+            {
+                key.SetValue("TargetTrainingValues", value);
+                OnPropertyChanged("TargetTrainingValues");
+            }
+        }
+
+        public string ADBAddress
+        {
+            get
+            {
+                return (string)key.GetValue("ADBAddress", "");
+            }
+            set
+            {
+                key.SetValue("ADBAddress", value);
+                OnPropertyChanged("ADBAddress");
+            }
+        }
+
+        public bool Debug
+        {
+            get
+            {
+                return (int)key.GetValue("Debug", 1) != 0;
+            }
+            set
+            {
+                key.SetValue("Debug", value, RegistryValueKind.DWord);
+                OnPropertyChanged("Debug");
+            }
+        }
+
+        public bool CheckUpdate
+        {
+            get
+            {
+                return (int)key.GetValue("CheckUpdate", 1) != 0;
+            }
+            set
+            {
+                key.SetValue("CheckUpdate", value, RegistryValueKind.DWord);
+                OnPropertyChanged("CheckUpdate");
+            }
+        }
+
+
+        public string Job
+        {
+            get
+            {
+                return (string)key.GetValue("Job", "nurturing");
+            }
+            set
+            {
+                key.SetValue("Job", value);
+                OnPropertyChanged("Job");
+            }
+        }
+
+        public JobOptions JobOptions1
+        { get; set; }
+
+
+        public string Distance
+        {
+            get
+            {
+                return (string)key.GetValue("Distance", "short");
+            }
+            set
+            {
+                key.SetValue("Distance", value);
+                OnPropertyChanged("Distance");
+            }
+        }
+
+        public DistanceOptions DistanceOptions1
+        { get; set; }
+    }
+}
---
+++ auto_derby/launcher/launcher_zh-tw.ps1
@@ -0,0 +1,208 @@
+﻿$WorkspaceFolder = $PSScriptRoot | Split-Path -Parent | Split-Path -Parent
+$env:PYTHONIOENCODING = ""
+Start-Transcript "$WorkspaceFolder\launcher_zh-tw.log"
+
+$ErrorActionPreference = "Stop"
+Set-Location $WorkspaceFolder
+[System.Environment]::CurrentDirectory = $WorkspaceFolder
+
+
+try {
+    $version = & git.exe describe --dirty --always 
+}
+catch {
+}
+if (-not $version) {
+    $version = Get-Content "$WorkspaceFolder\version"
+}
+
+$host.ui.RawUI.WindowTitle = "auto-derby: $version"
+
+
+Add-Type –AssemblyName PresentationFramework
+
+[System.Windows.Window]$mainWindow = [Windows.Markup.XamlReader]::Load( (New-Object System.Xml.XmlNodeReader ([xml](Get-Content "$PSScriptRoot\launcher_zh-tw.xaml"))) )
+
+Add-Type -Language CSharp ([string](Get-Content "$PSScriptRoot\launcher_zh-tw.cs"))
+
+$data = New-Object NateScarlet.AutoDerby.DataContext -Property @{
+    DefaultSingleModeChoicesDataPath = [System.IO.Path]::GetFullPath("data/single_mode_choices.csv")
+    DefaultPythonExecutablePath      = . {
+        try {
+            & py.exe -3.8 -c 'import sys; print(sys.executable)'
+        }
+        catch {
+            
+        }
+    }
+}
+$mainWindow.DataContext = $data
+
+$mainWindow.Content.FindName('startButton').add_Click( 
+    {
+        $mainWindow.DialogResult = $true
+        $mainWindow.Close()
+    }
+)
+$mainWindow.Content.FindName('chooseSingleModeChoicesDataPathButton').add_Click( 
+    {
+        $dialog = New-Object Microsoft.Win32.SaveFileDialog -Property @{
+            Title            = "Choose single mode choices"
+            AddExtension     = $true
+            DefaultExt       = ".json"
+            Filter           = "CSV data file|*.csv|Legacy JSON data file|*.json|Any file|*.*"
+            FileName         = $data.SingleModeChoicesDataPath
+            InitialDirectory = . {
+                try {
+                    Split-Path $data.SingleModeChoicesDataPath -Parent
+                }
+                catch {
+                } 
+            }
+        }
+        if ($dialog.ShowDialog()) {
+            $data.SingleModeChoicesDataPath = $dialog.FileName
+        } 
+    }
+)
+$mainWindow.Content.FindName('choosePythonExecutablePathButton').add_Click( 
+    {
+        $dialog = New-Object Microsoft.Win32.OpenFileDialog -Property @{
+            Title            = "Choose python executable"
+            Filter           = "Executable|*.exe|Any file|*.*"
+            FileName         = $data.PythonExecutablePath
+            InitialDirectory = . {
+                try {
+                    Split-Path $data.PythonExecutablePath -Parent
+                }
+                catch {
+                }
+            }
+        }
+        if ($dialog.ShowDialog()) {
+            $data.PythonExecutablePath = $dialog.FileName
+        }
+    }
+)
+
+$mainWindow.Content.FindName('selectPluginButton').add_Click( 
+    {
+        $env:AUTO_DERBY_PLUGINS = $data.Plugins
+        & $data.PythonExecutablePath "$PSScriptRoot\select_plugin.py" | ForEach-Object {
+            Write-Host $_
+            if ($_.StartsWith("AUTO_DERBY_PLUGINS=")) {
+                $data.Plugins = $_.Substring(19)
+            }
+        }
+    }
+)
+
+
+if (-not $mainWindow.ShowDialog()) {
+    "Cancelled"
+    Exit 0
+}
+
+$data | Format-List -Property (
+    "Job",
+    "Debug", 
+    "CheckUpdate",
+    "PythonExecutablePath",
+    "SingleModeChoicesDataPath",
+    "PauseIfRaceOrderGt",
+    "Plugins",
+    "Distance",
+    "ADBAddress",
+    @{
+        Name       = "Version"
+        Expression = { $version }
+    }, 
+    @{
+        Name       = "Python Version"
+        Expression = { & "$($Data.PythonExecutablePath)" -VV }
+    }
+)
+
+
+& "$WorkspaceFolder/auto_derby/launcher/migrate_data.ps1"
+if ($data.Debug) {   
+    $env:DEBUG = "auto_derby"
+    $env:AUTO_DERBY_LAST_SCREENSHOT_SAVE_PATH = "debug/last_screenshot.png"
+    $env:AUTO_DERBY_OCR_IMAGE_PATH = "debug/ocr_images"
+    $env:AUTO_DERBY_SINGLE_MODE_EVENT_IMAGE_PATH = "debug/single_mode_event_images"
+    $env:AUTO_DERBY_SINGLE_MODE_TRAINING_IMAGE_PATH = "debug/single_mode_training_images"
+    $env:AUTO_DERBY_WEB_LOG_BUFFER_PATH = "debug/log.jsonl"
+    $env:AUTO_DERBY_WEB_LOG_IMAGE_PATH = "debug/images"
+    & "$WorkspaceFolder/auto_derby/launcher/rotate_debug_data.ps1"
+}
+
+if ($data.CheckUpdate) {
+    $env:AUTO_DERBY_CHECK_UPDATE = "true"
+}
+
+if ($data.SingleModeChoicesDataPath) {
+    $env:AUTO_DERBY_SINGLE_MODE_CHOICE_PATH = $data.SingleModeChoicesDataPath
+}
+
+$env:AUTO_DERBY_PAUSE_IF_RACE_ORDER_GT = $data.PauseIfRaceOrderGt
+$env:AUTO_DERBY_PLUGINS = $data.Plugins
+$env:AUTO_DERBY_ADB_ADDRESS = $data.ADBAddress
+$env:AUTO_DERBY_SINGLE_MODE_TARGET_DISTANCE = $data.Distance
+
+$requireAdmin = (-not $data.ADBAddress)
+
+$verb = "open"
+if ($requireAdmin) {
+    $verb = "runAs"
+}
+
+$command = @"
+title auto-derby: $version
+cd /d "$WorkspaceFolder"
+set "DEBUG=$($env:DEBUG)"
+set "AUTO_DERBY_ADB_ADDRESS=$($env:AUTO_DERBY_ADB_ADDRESS)"
+set "AUTO_DERBY_CHECK_UPDATE=$($env:AUTO_DERBY_CHECK_UPDATE)"
+set "AUTO_DERBY_LAST_SCREENSHOT_SAVE_PATH=$($env:AUTO_DERBY_LAST_SCREENSHOT_SAVE_PATH)"
+set "AUTO_DERBY_OCR_IMAGE_PATH=$($env:AUTO_DERBY_OCR_IMAGE_PATH)"
+set "AUTO_DERBY_PAUSE_IF_RACE_ORDER_GT=$($env:AUTO_DERBY_PAUSE_IF_RACE_ORDER_GT)"
+set "AUTO_DERBY_PLUGINS=$($env:AUTO_DERBY_PLUGINS)"
+set "AUTO_DERBY_SINGLE_MODE_CHOICE_PATH=$($env:AUTO_DERBY_SINGLE_MODE_CHOICE_PATH)"
+set "AUTO_DERBY_SINGLE_MODE_EVENT_IMAGE_PATH=$($env:AUTO_DERBY_SINGLE_MODE_EVENT_IMAGE_PATH)"
+set "AUTO_DERBY_SINGLE_MODE_TARGET_DISTANCE=$($env:AUTO_DERBY_SINGLE_MODE_TARGET_DISTANCE)"
+set "AUTO_DERBY_SINGLE_MODE_TRAINING_IMAGE_PATH=$($env:AUTO_DERBY_SINGLE_MODE_TRAINING_IMAGE_PATH)"
+set "AUTO_DERBY_WEB_LOG_BUFFER_PATH=$($env:AUTO_DERBY_WEB_LOG_BUFFER_PATH)"
+set "AUTO_DERBY_WEB_LOG_IMAGE_PATH=$($env:AUTO_DERBY_WEB_LOG_IMAGE_PATH)"
+"$($Data.PythonExecutablePath)" -m auto_derby $($data.Job)
+start "auto-derby launcher" cmd.exe /c .\launcher_zh-tw.cmd
+exit
+"@
+
+"command: "
+$command
+""
+Start-Process cmd.exe -Verb $verb -ArgumentList @(
+    "/K",
+    ($command -split "`n" -join " && ")
+)
+
+
+if ($data.Debug) {
+    Remove-Item -Recurse -Force trash.local
+
+    "Installed packages: "
+    
+    & cmd.exe /c "`"$($Data.PythonExecutablePath)`" -m pip list 2>&1" | Select-String (
+        '^opencv-python\b',
+        '^opencv-contrib-python\b', 
+        '^pywin32\b', 
+        '^numpy\b',
+        '^Pillow\b',
+        '^mouse\b',
+        '^cast-unknown\b',
+        '^adb-shell\b'
+    )
+    ""
+}
+
+
+Stop-Transcript
